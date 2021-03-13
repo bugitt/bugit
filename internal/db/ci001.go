de2a4abcd39ed1975e9cc30af38bda3bc6e0ad49
loheagn <linan_123@buaa.edu.cn>
2021-03-13 20:30:12 +0800

    简化代码写法

--- internal/db/ci001.go
+++ internal/db/ci001.go
@@ -7,8 +7,8 @@ func (ptask *PipeTask) CI001() error {
 	// load repo
 	if err := ptask.LoadRepo(); err != nil {
 		return err
-	} else {
-		log.Info("load repo success for CI task: %d", ptask.ID)
 	}
+	log.Info("load repo success for CI task: %d", ptask.ID)
+
 	return nil
 }
