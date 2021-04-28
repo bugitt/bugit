8c58bd781bffbf1c7d2ab0699f25954312d15167
loheagn <loheagn@icloud.com>
2021-04-28 19:22:34 +0800

    ci001中产生的error加上相应的前缀

--- internal/db/ci001.go
+++ internal/db/ci001.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	log "unknwon.dev/clog/v2"
 )
@@ -28,25 +29,25 @@ func prepareCICtx(ptask *PipeTask, c context.Context) (*CIContext, error) {
 func (ptask *PipeTask) CI001(c context.Context) error {
 	context, err := prepareCICtx(ptask, c)
 	if err != nil {
-		return err
+		return fmt.Errorf("prepare ci context error: %w", err)
 	}
 
 	// load repo
 	if err := ptask.LoadRepo(context); err != nil {
-		return err
+		return fmt.Errorf("load repo error: %w", err)
 	}
 	log.Info("load repo success for CI task: %d", ptask.ID)
 	context.path = ptask.Pipeline.CIPath
 
 	// static code validation
 	if err := ptask.Validation(context); err != nil {
-		return err
+		return fmt.Errorf("validate error: %w", err)
 	}
 	log.Info("static code validation success for CI task: %d", ptask.ID)
 
 	// build image
 	if err := ptask.Build(context); err != nil {
-		return err
+		return fmt.Errorf("build error: %w", err)
 	}
 	log.Info("build image success for CI task: %d", ptask.ID)
 
@@ -55,13 +56,13 @@ func (ptask *PipeTask) CI001(c context.Context) error {
 
 	// push image
 	if err := ptask.Push(context); err != nil {
-		return err
+		return fmt.Errorf("push image error: %w", err)
 	}
 	log.Info("push image success for CI task: %d", ptask.ID)
 
 	// deploy image
 	if err := ptask.Deploy(context); err != nil {
-		return err
+		return fmt.Errorf("deploy to kubernetes error: %w", err)
 	}
 	log.Info("deploy success for CI task: %d", ptask.ID)
 
