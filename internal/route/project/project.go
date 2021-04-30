abf9fadab7383076379f78302166a6a79adbfd88
loheagn <loheagn@icloud.com>
2021-04-30 16:04:24 +0800

    完成project主页仓库列表的绘制

--- internal/route/project/project.go
+++ internal/route/project/project.go
@@ -1,11 +1,25 @@
 package project
 
-import "git.scs.buaa.edu.cn/iobs/bugit/internal/context"
+import (
+	"net/http"
+
+	"git.scs.buaa.edu.cn/iobs/bugit/internal/context"
+)
 
 const (
 	HomeView = "project/home"
 )
 
 func Home(c *context.Context) {
+	tab := c.Query("tab")
+	c.Data["TabName"] = tab
+	if tab == "" || tab == "repo" {
+		repos, err := c.Project.Project.GetRepos()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.Data["Repos"] = repos
+	}
 	c.Success(HomeView)
 }
